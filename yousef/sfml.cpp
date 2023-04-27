fdc7453eb5ba2e3d2a1c66890df6790b80b57fab
yousef9384 <131217266+yousef9384@users.noreply.github.com>
2023-04-28 00:01:28 +0200

    Add files via upload

--- yousef/sfml.cpp
+++ yousef/sfml.cpp
@@ -4,6 +4,8 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Audio.hpp>
+#include "button.cpp"
+
 using namespace std;
 
 string getCurrentTime()
@@ -15,16 +17,40 @@ string getCurrentTime()
     ss << local.tm_hour << ":" << local.tm_min << ":" << local.tm_sec;
     return ss.str();
 }
-void displayTime(sf::RenderWindow& window, sf::Font& font)
+void displayText(sf::RenderWindow& window, sf::Font& font,const string& str
+    , const int char_size,const int ch_Y, const int ch_X)
 {
     sf::Text text;
     text.setFont(font);
-    text.setCharacterSize(26);
+    text.setCharacterSize(char_size);
     text.setFillColor(sf::Color::White);
-    text.setPosition(10, 10);
+    text.setPosition(ch_X, ch_Y);
     string currentTime = getCurrentTime();
-    text.setString(currentTime);
+    text.setString(str);
     window.draw(text);
+}
+void displayLoading(sf::RenderWindow& window, const std::string& fileName)
+{
+
+    sf::Texture loadingTexture;
+    if (!loadingTexture.loadFromFile(fileName))
+    {
+        std::cout << "Error loading loading image" << std::endl;
+    }
+    sf::Sprite loadingSprite;
+    loadingSprite.setTexture(loadingTexture);
+
+    sf::Vector2u imageSize = loadingTexture.getSize();
+    sf::Vector2u windowSize = window.getSize();
+
+    float scaleX = static_cast<float>(windowSize.x) / imageSize.x;
+    float scaleY = static_cast<float>(windowSize.y) / imageSize.y;
+
+    loadingSprite.setScale(scaleX, scaleY);
+
+    window.draw(loadingSprite);
+
+
 }
 
 int main()
@@ -32,10 +58,11 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "wonder world");
     sf::Texture backgroundTexture;
 
-    if (!backgroundTexture.loadFromFile("background.jpg"))
-    {
-        cout << "Error loading background image" <<endl;
-    }
+    sf::Font font;
+
+    font.loadFromFile("arial.ttf");
+    bool enterPressed = false;
+    backgroundTexture.loadFromFile("background.jpg");
 
 
     sf::Sprite backgroundSprite;
@@ -46,12 +73,41 @@ int main()
     float scaleX = static_cast<float>(windowSize.x) / imageSize.x;
     float scaleY = static_cast<float>(windowSize.y) / imageSize.y;
 
+    Button playButton("Play", font, 40, sf::Color::Green, sf::Vector2f(100,100));
+    Button aboutButton("about", font, 40, sf::Color::Green, sf::Vector2f(100, 200));
+    Button creditsButton("credits", font, 40, sf::Color::Green, sf::Vector2f(100, 300));
+    Button exitButton("exit", font, 40, sf::Color::Green, sf::Vector2f(100, 400));
     backgroundSprite.setScale(scaleX, scaleY);
 
 
     while (window.isOpen())
     {
         sf::Event event;
+        //
+        if (playButton.isMouseOver(window)) { 
+            playButton.setTextFillcolor(sf::Color::White);
+        }
+        else {     playButton.setShapeFillColor(sf::Color::Blue);
+        }
+        if (aboutButton.isMouseOver(window)) {
+            aboutButton.setTextFillcolor(sf::Color::White);
+        }
+        else {
+            aboutButton.setShapeFillColor(sf::Color::Blue);
+        }
+        if (creditsButton.isMouseOver(window)) {
+            creditsButton.setTextFillcolor(sf::Color::White);
+        }
+        else {
+            creditsButton.setShapeFillColor(sf::Color::Blue);
+        }
+        if (exitButton.isMouseOver(window)) {
+            exitButton.setTextFillcolor(sf::Color::White);
+        }
+        else {
+            exitButton.setShapeFillColor(sf::Color::Blue);
+        }
+        //
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed ||
@@ -59,21 +115,41 @@ int main()
             {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Enter)
+                {
+                    enterPressed = true;
+                }
+            }
+            
         }
 
-
+        string currentTime = getCurrentTime();
         sf::Font font;
-        if (!font.loadFromFile("arial1.ttf"))
-        {
-            std::cout << "Error loading font file" << std::endl;
-        }
 
+        font.loadFromFile("arial.ttf");
 
         window.clear(sf::Color::Black);
-        window.draw(backgroundSprite);
-        displayTime(window, font);
+        if (enterPressed)
+        {
+            window.draw(backgroundSprite);
+            playButton.draw(window);
+            displayText(window, font,currentTime,35,10, 650);
+            aboutButton.draw(window);
+            creditsButton.draw(window);
+            exitButton.draw(window);
+
+        }
+        else
+        {
+            displayLoading(window, "loading.png");
+            displayText(window, font, "wonder world", 50, 200, 200);
+        }
+  
         window.display();
     }
+
 
     return 0;
 }
